eed48ddd2f6453b0b6bde19a4e658a5087e07b9a
Joj Macho <zingakay@gmail.com>
2023-05-24 15:56:02 +0200

    geometric series program

---
+++ geometric-series/README.md
@@ -0,0 +1,14 @@
+# Geometric Series
+
+## Description
+
+This is a Python program that generates a geometric series.
+
+## How it Works
+
+## Program Input & Output
+
+When you run the program, `.py`, the output will look like this;
+
+```
+```
---
+++ geometric-series/geometric_series.py
@@ -0,0 +1,45 @@
+def generate_geometric_series(first_term, common_ratio, num_terms):
+    """
+    Generates a geometric series.
+    """
+    series = [first_term * common_ratio ** n for n in range(num_terms)]
+    return series
+
+
+def geometric_series_sum(first_term, common_ratio, num_terms):
+    """
+    Calculates the sum of a geometric series.
+    """
+    if common_ratio == 1:
+        series_sum = first_term * num_terms
+    else:
+        series_sum = (first_term * (1 - common_ratio ** num_terms)) / (1 - common_ratio)
+    return series_sum
+
+
+def main():
+    """
+    Main function to get user input, generate a geometric series, and calculate its sum.
+    """
+    print("Geometric Series Sum\n")
+
+    while True:
+        try:
+            first_term = float(input("Enter the first term: "))
+            common_ratio = float(input("Enter the common ratio: "))
+            num_terms = int(input("Enter the number of terms: "))
+            break
+        except ValueError:
+            print("Invalid input. Please enter valid numbers.")
+
+    geometric_series = generate_geometric_series(first_term, common_ratio, num_terms)
+    series_sum = geometric_series_sum(first_term, common_ratio, num_terms)
+
+    print("\nThe generated geometric series is:")
+    print(geometric_series)
+
+    print(f"\nThe sum of the geometric series is: {series_sum}")
+
+
+if __name__ == "__main__":
+    main()
