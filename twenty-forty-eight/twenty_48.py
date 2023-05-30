048ca031b745a4e0dbbc578b185db5b99d083b70
Joj Macho <zingakay@gmail.com>
2023-05-30 10:18:10 +0200

    2048 program

---
+++ twenty-forty-eight/twenty_48.py
@@ -0,0 +1,65 @@
+import random
+import sys
+
+
+BLANK = ''
+SIZE = 4
+GOAL = 2048
+
+
+def main():
+    '''This is the main function.'''
+
+    pass
+
+
+def generate_new_board():
+    '''Generates a new board data structure.'''
+
+    pass
+
+
+def draw_board(board):
+    '''Draws the board data structure on the screen.'''
+
+    pass
+
+
+def get_score(board):
+    '''Calculates the score based on the board data structure.'''
+
+    pass
+
+
+def combine_tiles_in_column(column):
+    '''Combines the tiles in a column and returns the updated column.'''
+
+    pass
+
+
+def make_move(board, move):
+    '''Carries out the move on the board and returns the updated board.'''
+
+    pass
+
+
+def add_new_tile(board):
+    '''Adds a new tile (2 or 4) to a random empty space on the board.'''
+    
+    pass
+
+
+def is_board_full(board):
+    '''Checks if the board is full and returns True or False.'''
+    
+    pass
+
+
+def ask_for_player_move():
+    '''Asks the player for a move and returns the valid move.'''
+    
+    pass
+
+
+if __name__ == '__main__':
+    main()
