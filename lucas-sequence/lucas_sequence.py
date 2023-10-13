c9a73dc9435c4e4ee3a3eb686535e0be83c6dd69
Joj Macho <zingakay@gmail.com>
2023-10-13 14:30:08 +0200

    Add input validation function

--- lucas-sequence/lucas_sequence.py
+++ lucas-sequence/lucas_sequence.py
@@ -1,75 +1,74 @@
-
 def main():
     '''Main function to generate Lucas numbers.'''
 
     print('\nLucas Number Generator\n')
 
     while True:
+        # Display menu options
+        print('Choose an option:')
         print('1. Generate nth Lucas number')
         print('2. Generate Lucas sequence up to nth term')
-        print('3. Enter (q)uit to exit program')
+        print('3. Exit program')
 
-        choice = input('Enter your choice:\n> ')
+        choice = get_valid_input('Enter your choice: ')
 
-        if choice == '1':
-            generate_nth_lucas_number()
-        elif choice == '2':
-            generate_lucas_sequence()
-        elif choice == '3' or choice.startswith('q'):
+        if choice == 1:
+            # Generate the nth Lucas Number
+            number = get_valid_input('Enter a positive integer: ')
+            print(f'The {number}th Lucas number is: {get_nth_lucas_number(number)}\n')
+
+        elif choice == 2:
+            # Generate Lucas sequence up to the nth term.
+            number = get_valid_input('Enter a positive integer: ')
+            lucas_sequence = generate_lucas_sequence(number)
+            print(f'The Lucas sequence up to the {number}th term is:\n{lucas_sequence}.\n')
+
+        elif choice == 3:
+            # Exit program
             print('Bye.')
             break
         else:
             print('Invalid choice. Please enter a valid option.')
 
-def generate_nth_lucas_number():
-    '''Function to generate the nth Lucas number.'''
 
+def get_valid_input(message):
+    '''Get a valid input from the user.'''
+    # Validate user input
     while True:
         try:
-            n = int(input('Enter the value of n: '))
-            break
+            user_input = int(input(message))
+            if user_input >= 0:
+                return user_input
+            else:
+                print('Invalid input. Please enter a non-negative integer.\n')
         except ValueError:
             print('Invalid input. Please enter a valid integer.\n')
 
-    lucas_number = get_nth_lucas_number(n)
-
-    print(f'The {n}th Lucas number is: {lucas_number}\n')
-
-
-def generate_lucas_sequence():
-    '''Function to generate the Lucas sequence up to the nth term.'''
-
-    while True:
-        try:
-            n = int(input('Enter the value of n: '))
-            break
-        except ValueError:
-            print('Invalid input. Please enter a valid integer.\n')
-
-    lucas_sequence = get_lucas_sequence(n)
-
-    print(f'The Lucas sequence up to the {n}th term is:\n{lucas_sequence}.\n')
 
 def get_nth_lucas_number(n):
     '''Returns the nth Lucas number.'''
-
+    # Base cases for 0th and 1st Lucas numbers
     if n == 0:
         return 2
     elif n == 1:
         return 1
     else:
+        # Initialize the first two Lucas numbers
         a, b = 2, 1
+        # Calculate subsequent Lucas numbers using the Lucas sequence formula
         for _ in range(2, n + 1):
-            a, b = b, a + b
+            a, b = b, a + b  # Update values using the formula L(n) = L(n-1) + L(n-2)
         return b
 
-def get_lucas_sequence(n):
-    '''Returns the Lucas sequence up to the nth term.'''
 
-    sequence = []
+def generate_lucas_sequence(n):
+    '''Generates the Lucas sequence up to the nth term.'''
+    lucas_sequence = []
     for i in range(n + 1):
-        sequence.append(get_nth_lucas_number(i))
-    return sequence
+        lucas_sequence.append(get_nth_lucas_number(i))
+
+    return lucas_sequence
+
 
 if __name__ == '__main__':
     main()
